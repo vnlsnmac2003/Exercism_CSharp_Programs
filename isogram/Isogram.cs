8f2eb41e10951d198d7712a7b3dadd64abbfb64c
vnlsnmac2003 <vnlsnmac2003@aol.com>
2019-08-22 16:06:47 +0100

    Isogram Exercise Complete

---
+++ isogram/Isogram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        IEnumerable<char> input = word.ToLower().Where(char.IsLetter); //get all lowercase letters
+        return input.Distinct().Count() == input.Count(); //Find out whether all letters are unique
+    }
+}
